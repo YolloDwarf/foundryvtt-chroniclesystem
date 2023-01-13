66b8a49a9cf8c1fbea0a840dec00c4e42680291b
Nicolò Antonucci <nicolo.antonucci@i-tel.it>
2023-01-13 16:04:31 +0100

    implementazione roll custom

--- module/rolls/cs-roll.js
+++ module/rolls/cs-roll.js
@@ -1,36 +1,57 @@
 import SystemUtils from "../utils/systemUtils.js";
 
+export const ROLL_TYPE = {
+    COMBAT: 'combat',
+    INTRIGUE: 'intrigue',
+    OTHER: 'other'
+}
+
 export class CSRoll {
-    constructor(title, formula) {
-        this.formula = formula;
+    constructor(actor, title, skill, rollData) {
+        this.rollData = rollData;
+        this.skill = skill;
         this.title = title;
-        this.entityData = undefined;
+        this.actor = actor;
         this.rollCard  = "systems/chroniclesystem/templates/chat/cs-stat-rollcard.html";
         this.results = [];
+        this.type = this._getRollType()
     }
 
-    async doRoll(actor, async = true) {
-        if (this.formula.pool - this.formula.dicePenalty <=0 ) {
+    _getRollType() {
+        switch (this.skill) {
+            case 'fighting':
+            case 'marksmanship':
+                return ROLL_TYPE.COMBAT;
+            case 'deception':
+            case 'persuasion':
+                return ROLL_TYPE.INTRIGUE;
+            default: 
+                return ROLL_TYPE.OTHER;
+        }
+    }
+
+    async doRoll(async = true) {
+        const testDice = this.rollData.pool - this.rollData.dicePenalty;
+        
+        if (testDice < 0) {
             ui.notifications.info(SystemUtils.localize("CS.notifications.dicePoolInvalid"));
             return null;
         }
-        const pool = Math.max(this.formula.pool, 1);
-        const dices = pool + this.formula.bonusDice;
-        let dieRoll = new Die({faces: 6, number: dices});
-        await dieRoll.evaluate({async : async});
-        this.results = dieRoll.results;
 
-        let reRollFormula = "r"+this.formula.reRoll+"=1";
-        dieRoll.reroll(reRollFormula);
+        let rerollFormula = '';
+        if (this.rollData.reRoll) {
+            rerollFormula = this.rollData.reRoll.reduce((acc, curr) => acc + `r${curr.number ? curr.number + '=' : ''}${curr.value}`,'');
+        }
+        
+        const bonusDice = Math.max(0, this.rollData.bonusDice)
+        let roll = await new Roll(`
+        ${testDice + bonusDice}
+        d6${rerollFormula}kh${testDice}${this.rollData.modifier 
+        ? '+' + this.rollData.modifier : ''}
+        `).evaluate({async: true});
+        
 
-        dieRoll.keep('kh' + Math.max(this.formula.pool - this.formula.dicePenalty, 0));
-
-        const plus = new OperatorTerm({operator: "+"});
-        plus.evaluate();
-        const bonus = new NumericTerm({number: this.formula.modifier});
-        bonus.evaluate();
-
-        let resultRoll = Roll.fromTerms([dieRoll, plus, bonus]);
+        // TO DO: here
         const messageId = this.formula.isUserChanged ? "CS.chatMessages.customRoll" : "CS.chatMessages.simpleRoll";
         let flavor =  SystemUtils.format(messageId, {name: actor.name, test: this.title});
         resultRoll.toMessage({
